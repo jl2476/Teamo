dfdfdec7942b884fd8c6a1cf977fdf4d7a52cc17
John <jl2476@rit.edu>
2025-09-19 19:12:59 -0400

    first commit

---
+++ Teamo/Teamo/src/main/java/com/example/Teamo/TeamoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Teamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamoApplication.class, args);
+	}
+
+}
---
+++ Teamo/Teamo/src/test/java/com/example/Teamo/TeamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Teamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
